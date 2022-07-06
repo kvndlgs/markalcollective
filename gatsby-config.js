0507155bfb7995719070db3d203d67a14d440e92
kvndlgs <poissedonn@gmail.com>
2022-07-06 10:57:32 -0400

    ayyyy

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,14 +12,12 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-open-graph-images`,
     `gatsby-transformer-json`,
-    },
     {
     resolve: `gatsby-source-filesystem`,
     options: {
        name: 'data',
        path: `${__dirname}/src/data/`,
     },
-    
     },
     {
       resolve: `gatsby-plugin-styled-components`,
